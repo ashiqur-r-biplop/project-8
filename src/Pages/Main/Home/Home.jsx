932fdf1db92e5ba0f897d5068833e56a0a306fc5
MdTalha707 <mdsagor0321@gmail.com>
2023-10-22 15:54:30 +0600

    work finished

--- src/Pages/Main/Home/Home.jsx
+++ src/Pages/Main/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Testimonial from "../../../Component/Main/Home/Testimonial";
+import HomeComponent from "../../../Component/Main/Home/HomeComponent";
 
 const Home = () => {
-  return <div>
-
-    <Testimonial></Testimonial>
-
-  </div>;
+  return (
+    <>
+      <HomeComponent></HomeComponent>
+    </>
+  );
 };
 
 export default Home;
