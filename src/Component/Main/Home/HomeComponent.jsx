47bfce9f212113e3cce2a27efecb7e48480f0d0f
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-22 18:45:45 +0600

    Banner Part Complete

--- src/Component/Main/Home/HomeComponent.jsx
+++ src/Component/Main/Home/HomeComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Testimonial from "./Testimonial.jsx";
 import OurMoto from "./OurMoto.jsx";
+import Banner from "./Banner.jsx";
 
 const HomeComponent = () => {
   return (
     <div>
+      <Banner></Banner>
       <Testimonial></Testimonial>
       <OurMoto></OurMoto>
     </div>
