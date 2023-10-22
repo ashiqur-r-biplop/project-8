ee1a0303ee932899ff015313fd95f2e13fcb478b
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-22 19:21:28 +0600

    Added Count Section

--- src/Component/Main/Home/Count.jsx
+++ src/Component/Main/Home/Count.jsx
@@ -4,7 +4,7 @@ import NumberCounter from 'number-counter';
 
 const Count = () => {
     return (
-        <div className="mx-12 mt-8">
+        <div className="mx-12 mt-8 pb-8">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                 <div className='brand-color bg-black text-center p-8 rounded-lg'>
                     <h1 className="text-4xl"><NumberCounter end={100} delay={1} className="increment" preFix="" postFix="Million+" /></h1>
