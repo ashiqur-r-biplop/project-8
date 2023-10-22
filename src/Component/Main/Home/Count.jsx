9227ee852e47c53e1279d1a6f753976b1f0e4061
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-22 19:17:53 +0600

    Count Part Complete

---
+++ src/Component/Main/Home/Count.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Banner.css';
+import NumberCounter from 'number-counter';
+
+const Count = () => {
+    return (
+        <div className="mx-12 mt-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div className='brand-color bg-black text-center p-8 rounded-lg'>
+                    <h1 className="text-4xl"><NumberCounter end={100} delay={1} className="increment" preFix="" postFix="Million+" /></h1>
+                    <p className="text-white text-2xl mt-4">Tickets Sold</p>
+                </div>
+                <div className='brand-color bg-black text-center p-8 rounded-lg'>
+                    <h1 className="text-4xl"><NumberCounter end={300} delay={1} className="increment" preFix="" postFix="+" /></h1>
+                    <p className="text-white text-2xl mt-4">Routes</p>
+                </div>
+                <div className='brand-color bg-black text-center p-8 rounded-lg'>
+                    <h1 className="text-4xl"><NumberCounter end={10} delay={1} className="increment" preFix="" postFix="Million+" /></h1>
+                    <p className="text-white text-2xl mt-4">Happy Users</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Count;
--- src/Component/Main/Home/HomeComponent.jsx
+++ src/Component/Main/Home/HomeComponent.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Testimonial from "./Testimonial.jsx";
 import OurMoto from "./OurMoto.jsx";
 import Banner from "./Banner.jsx";
+import Count from "./Count.jsx";
 
 const HomeComponent = () => {
   return (
     <div>
       <Banner></Banner>
+      <Count></Count>
       <Testimonial></Testimonial>
       <OurMoto></OurMoto>
     </div>
