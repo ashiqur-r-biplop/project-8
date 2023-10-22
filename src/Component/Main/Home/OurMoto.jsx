c3bd2e70695c018cee9d955bd429f4f76804f4f6
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-22 19:52:04 +0600

    Update Two Column Section

--- src/Component/Main/Home/OurMoto.jsx
+++ src/Component/Main/Home/OurMoto.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import bus1 from '../../../../src/assets/bus1.gif'
 import bus2 from "../../../../src/assets/bus6.jpeg";
 import { Link } from 'react-router-dom';
-import  style from './OurMoto.module.css';
+import style from './OurMoto.module.css';
 
 const OurMoto = () => {
   return (
@@ -10,19 +9,19 @@ const OurMoto = () => {
       <div className="container w-full grid md:grid-cols-2   grid-cols-1 my-4 md:my-0 ">
         <div className="w-full  flex items-center">
           <div className="md:pl-8 px-2 ">
-            <h1 className="uppercase text-2xl md:text-4xl font-semibold text-[#9333ea]">
+            <h1 className="text-2xl md:text-4xl brand-color font-bold">
               Bus Tickets
             </h1>
-            <h2 className="uppercase text-[#9333ea] font-semibold text-lg md:text-2xl">
+            <h2 className="font-semibold text-lg md:text-2xl mt-2">
               Online Service
             </h2>
-            <p className="md:my-5 my-3 text-sm md:text-normal text-[#9333ea]">
+            <p className="md:my-5 my-3 text-sm md:text-normal text-gray-600">
               We provide you a easy and husle free ticket booking service . Get
               Your Ticket in your Pocket No more husle in line.now you can go
               home easily
             </p>
-            <Link to="/book-ticker">
-              <button className=" text-white px-2 md:px-3 md:py-2 py-1 rounded  bg-[#9333ea]">
+            <Link to="/book-ticket">
+              <button className="btn brand-btn">
                 Book Now
               </button>
             </Link>
@@ -32,7 +31,7 @@ const OurMoto = () => {
           <img
             src={bus2}
             className={style.slide}
-                  />
+          />
         </div>
       </div>
     </div>
