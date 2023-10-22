10cb7e3707ac8bb4a332eb4aa8d1c7a547906584
MdTalha707 <mdsagor0321@gmail.com>
2023-10-22 13:28:56 +0600

    first commit

--- src/Pages/Main/Home/Home.jsx
+++ src/Pages/Main/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Testimonial from "../../../Component/Main/Home/Testimonial";
 
 const Home = () => {
-  return <div></div>;
+  return <div>
+
+    <Testimonial></Testimonial>
+
+  </div>;
 };
 
 export default Home;
