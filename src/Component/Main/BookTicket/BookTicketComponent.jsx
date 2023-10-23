3338ca53bb252f11cf127b39bc6677fc4bbd4085
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-10-23 23:24:11 +0600

    added book ticket form

--- src/Component/Main/BookTicket/BookTicketComponent.jsx
+++ src/Component/Main/BookTicket/BookTicketComponent.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import SeatSelection from './SeatSelection';
+import BookTicket from '../../../Pages/Main/BookTicket/BookTicket';
 
 const BookTicketComponent = () => {
     return (
         <div className="grid md:grid-cols-2 mx-auto min-width-[1200px]">
-            <h2>Hello</h2>
+            <BookTicket></BookTicket>
             <SeatSelection></SeatSelection>
         </div>
     );
