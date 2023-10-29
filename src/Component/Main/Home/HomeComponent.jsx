8fcb4856843ff1ac3ee343b8ff20670ed0775333
MdTalha707 <mdsagor0321@gmail.com>
2023-10-29 15:31:01 +0600

    addd steps to book ticket

--- src/Component/Main/Home/HomeComponent.jsx
+++ src/Component/Main/Home/HomeComponent.jsx
@@ -4,6 +4,7 @@ import OurMoto from "./OurMoto.jsx";
 import Banner from "./Banner.jsx";
 import Count from "./Count.jsx";
 import HomeMarquee from "./HomeMarquee.jsx";
+import TicketBookingSteps from "./TicketBookingSteps.jsx";
 const HomeComponent = () => {
   return (
     <div className="mt-[20px]">
@@ -12,6 +13,7 @@ const HomeComponent = () => {
       <Count></Count>
       <OurMoto></OurMoto>
       <Testimonial></Testimonial>
+      <TicketBookingSteps></TicketBookingSteps>
     </div>
   );
 };
