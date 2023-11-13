f734d5d4d212f3234ad65a8b5418bc846aef00be
AhaD <abuahad35188@gmail.com>
2023-11-13 13:31:05 +0600

    some change of Featured.jsx component

--- src/Component/Main/Home/Featured.jsx
+++ src/Component/Main/Home/Featured.jsx
@@ -10,29 +10,29 @@ import { Link } from "react-router-dom";
 
 const Featured = () => {
   return (
-    <div className="w-full  py-6  ">
-      <div className="grid md:grid-cols-2 ">
+    <div className=" w-full   mx-auto py-6 banner-content ">
+      <div className="grid md:grid-cols-2 p-2 ">
         <div className="relative">
           <div className="">
-            <img src={busF} className="w-full min-h-full" />
+            <img src={busF} className="w-full min-h-full opacity-[0.8] brightness-[0.8] " />
           </div>
-          <div className="hidden md:block absolute rounded-md px-2 py-2 bottom-10  w-4/12 f-4/12 bg-orange-600 text-white">
+          <div className="hidden md:block absolute rounded-md px-2 py-2 bottom-10  w-4/12 f-4/12 bg-orange-600 text-white ms-5">
             <h1>We Provide you Best Bus</h1>
-            <p className="text-xs">
+            <p className="text-xs text-gray-700">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Consequatur, culpa?
             </p>
-            <Link className="text-xs underline" to="">
+            <Link className="text-xs underline text-red-300" to="">
               View Bus
             </Link>
           </div>
         </div>
-        <div className="bg-black p-4 md:p-6 w-full h-full">
+        <div className="bg-gray-800 p-4 md:p-6 w-full h-full">
           <p className="brand-color text-xl capitalize text-center">Why choose us</p>
-          <h1 className="text-white mt-2 md:text-4xl  ">
+          <h1 className="text-orange-600 mt-2 md:text-4xl  ">
             We are experts in bus charters company since 1989!
           </h1>
-          <p className="text-white text-[24px] py-2 ">
+          <p className="text-gray-600 text-[24px] py-2 ">
             In this edition of our newsletter, we want to shine a spotlight on
             you, our valued customer. Your trust in our Company means the world
             to us, and we want to take a moment to celebrate your loyalty and
@@ -42,40 +42,40 @@ const Featured = () => {
             <div className="">
               <div className="flex  gap-4 mb-3 md:mb-0 ms-5">
                 <div className="flex items-center">
-                  <BsFillBusFrontFill className="brand-color  text-3xl" />
+                  <BsFillBusFrontFill className="text-orange-700 text-3xl" />
                 </div>
                 <div>
-                  <span className="text-xl md:text-3xl text-white">40 +</span>
-                  <p className="text-white text-xs">Buses Ready</p>
+                  <span className="text-xl md:text-3xl text-gray-600">40 +</span>
+                  <p className="text-gray-600 text-xs">Buses Ready</p>
                 </div>
               </div>
               <div className="flex md:mt-4 mb-3 md:mb-0 gap-4 ms-5">
                 <div className="flex items-center">
-                  <FaHandshake className="brand-color  text-3xl" />
+                  <FaHandshake className="text-orange-700  text-3xl" />
                 </div>
                 <div>
-                  <span className="text-xl md:text-3xl text-white">340 +</span>
-                  <p className="text-white text-xs">Booking Done</p>
+                  <span className="text-xl md:text-3xl text-gray-600">340 +</span>
+                  <p className="text-gray-600 text-xs">Booking Done</p>
                 </div>
               </div>
             </div>
             <div className="">
               <div className="flex  gap-4 mb-3 md:mb-0 ms-5">
                 <div className="flex items-center">
-                  <BsFillAwardFill className="brand-color  text-3xl" />
+                  <BsFillAwardFill className="text-orange-700  text-3xl" />
                 </div>
                 <div>
-                  <span className="text-xl md:text-3xl text-white">2940 +</span>
-                  <p className="text-white text-xs">Satisfied Customer</p>
+                  <span className="text-xl md:text-3xl text-gray-600">2940 +</span>
+                  <p className="text-gray-600 text-xs">Satisfied Customer</p>
                 </div>
               </div>
               <div className="flex md:mt-4 gap-4 mb-3 md:mb-0 ms-5">
                 <div className="flex items-center">
-                  <BsFillPeopleFill className="brand-color  text-3xl" />
+                  <BsFillPeopleFill className=" text-orange-700  text-3xl" />
                 </div>
                 <div>
-                  <span className="text-xl md:text-3xl text-white">20 +</span>
-                  <p className="text-white text-xs">Professional Team</p>
+                  <span className="text-xl md:text-3xl text-gray-600">20 +</span>
+                  <p className="text-gray-600 text-xs">Professional Team</p>
                 </div>
               </div>
             </div>
